d93ac5fc0ad57618bc1421d1b1d0e910a7ff09e8
hesams2002 <hesamsafari57@gmail.com>
2025-11-21 09:52:15 +0330

    clean code

--- sps.py
+++ sps.py
@@ -1,63 +1,65 @@
-'''
-sessior paper stone game let's play
-'''
+"""
+rock paper scissors game let's play
+"""
+
 import random
+
 end = True
 player_point = 0
 computer_point = 0
-while end :
-    print("AI =", computer_point,"YOU =", player_point)
+while end:
+    print("AI =", computer_point, "YOU =", player_point)
     answer = input("rock,paper and scissors?choose on of them and type: \n")
-    
-    if answer == "scissors" :
-        end = True
-    elif answer == "paper" :
-        end = True
-    elif answer == "rock" :
-        end == True
-    else :
+
+    if answer == "scissors":
+        pass
+    elif answer == "paper":
+        pass
+    elif answer == "rock":
+        pass
+    else:
         print("you did't choose invalid answer")
         continue
 
-    ai = random.randint(1,3)
-    if ai == 1 :
+    ai = random.randint(1, 3)
+    if ai == 1:
         computer_answer = "paper"
-    elif ai == 2 :
+    elif ai == 2:
         computer_answer = "scissors"
-    else :
+    else:
         computer_answer = "rock"
 
-    if computer_answer == answer :
+    if computer_answer == answer:
         print("unlucky !!!!!!!it's DRAW!!!!!!!")
-        print("computer answer= ",computer_answer)
-    elif computer_answer == "paper" and answer == "rock" :
+        print("computer answer= ", computer_answer)
+    elif computer_answer == "paper" and answer == "rock":
         print("!!!!!!!you LOSE!!!!!!!")
         computer_point += 1
-        print("computer answer= ",computer_answer)
-    elif computer_answer == "paper" and answer == "scissors" :
+        print("computer answer= ", computer_answer)
+    elif computer_answer == "paper" and answer == "scissors":
         print("!!!!!!!you WON!!!!!!!")
         player_point += 1
-        print("computer answer= ",computer_answer)
-    elif computer_answer == "scissors" and answer == "paper" :
+        print("computer answer= ", computer_answer)
+    elif computer_answer == "scissors" and answer == "paper":
         print("!!!!!!!you LOSE!!!!!!!")
         computer_point += 1
-        print("computer answer= ",computer_answer)
-    elif computer_answer == "scissors" and answer == "rock" :
+        print("computer answer= ", computer_answer)
+    elif computer_answer == "scissors" and answer == "rock":
         print("!!!!!!!you WON!!!!!!!")
         player_point += 1
-        print("computer answer= ",computer_answer)
-    elif computer_answer == "rock" and answer == "scissors" :
+        print("computer answer= ", computer_answer)
+    elif computer_answer == "rock" and answer == "scissors":
         print("!!!!!!!you LOSE!!!!!!!")
         computer_point += 1
-        print("computer answer= ",computer_answer)
-    elif computer_answer == "rock" and answer == "paper" :
+        print("computer answer= ", computer_answer)
+    elif computer_answer == "rock" and answer == "paper":
         print("!!!!!!!you WIN!!!!!!!")
         player_point += 1
-        print("computer answer= ",computer_answer)
-    if computer_point == 3 :
+        print("computer answer= ", computer_answer)
+    if computer_point == 3:
         print("!!!!!!the computer is the finalist WINNER!!!!!!!")
         end = False
-    elif player_point == 3 :
+    elif player_point == 3:
         print("!!!!!!!you are the finalist WINNER!!!!!!!")
         end = False
-print("!!!!!!! AI =", computer_point,"YOU =", player_point, "!!!!!!!")
+print("!!!!!!! AI =", computer_point, "YOU =", player_point, "!!!!!!!")
