0dec6a749ae47e2c865064f258b752ceef4c9e21
hesams2002 <hesamsafari57@gmail.com>
2025-11-21 09:36:54 +0330

    change to real name

--- sps.py
+++ sps.py
@@ -7,13 +7,13 @@ player_point = 0
 computer_point = 0
 while end :
     print("AI =", computer_point,"YOU =", player_point)
-    answer = input("sessior,paper or stone?choose on of them and type: \n")
+    answer = input("rock,paper and scissors?choose on of them and type: \n")
     
-    if answer == "sessior" :
+    if answer == "scissors" :
         end = True
     elif answer == "paper" :
         end = True
-    elif answer == "stone" :
+    elif answer == "rock" :
         end == True
     else :
         print("you did't choose invalid answer")
@@ -23,34 +23,34 @@ while end :
     if ai == 1 :
         computer_answer = "paper"
     elif ai == 2 :
-        computer_answer = "sessior"
+        computer_answer = "scissors"
     else :
-        computer_answer = "stone"
+        computer_answer = "rock"
 
     if computer_answer == answer :
         print("unlucky !!!!!!!it's DRAW!!!!!!!")
         print("computer answer= ",computer_answer)
-    elif computer_answer == "paper" and answer == "stone" :
+    elif computer_answer == "paper" and answer == "rock" :
         print("!!!!!!!you LOSE!!!!!!!")
         computer_point += 1
         print("computer answer= ",computer_answer)
-    elif computer_answer == "paper" and answer == "sessior" :
+    elif computer_answer == "paper" and answer == "scissors" :
         print("!!!!!!!you WON!!!!!!!")
         player_point += 1
         print("computer answer= ",computer_answer)
-    elif computer_answer == "sessior" and answer == "paper" :
+    elif computer_answer == "scissors" and answer == "paper" :
         print("!!!!!!!you LOSE!!!!!!!")
         computer_point += 1
         print("computer answer= ",computer_answer)
-    elif computer_answer == "sessior" and answer == "stone" :
+    elif computer_answer == "scissors" and answer == "rock" :
         print("!!!!!!!you WON!!!!!!!")
         player_point += 1
         print("computer answer= ",computer_answer)
-    elif computer_answer == "stone" and answer == "sessior" :
+    elif computer_answer == "rock" and answer == "scissors" :
         print("!!!!!!!you LOSE!!!!!!!")
         computer_point += 1
         print("computer answer= ",computer_answer)
-    elif computer_answer == "stone" and answer == "paper" :
+    elif computer_answer == "rock" and answer == "paper" :
         print("!!!!!!!you WIN!!!!!!!")
         player_point += 1
         print("computer answer= ",computer_answer)
